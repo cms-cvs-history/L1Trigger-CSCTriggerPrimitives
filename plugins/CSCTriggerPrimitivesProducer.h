19bc415c81ef121dbbfc1ef46ccd374291c62efb
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2013-05-21 12:02:25 +0000

    the infrastructure to be able to perform dphi and deta matching of ME1b LCTs and GEM trigger pads

--- plugins/CSCTriggerPrimitivesProducer.h
+++ plugins/CSCTriggerPrimitivesProducer.h
@@ -17,7 +17,7 @@
  *
  * \author Slava Valuev, UCLA.
  *
- * $Id: CSCTriggerPrimitivesProducer.h,v 1.4.2.1 2012/05/16 00:31:24 khotilov Exp $
+ * $Id: CSCTriggerPrimitivesProducer.h,v 1.5 2012/12/05 21:16:16 khotilov Exp $
  *
  */
 
@@ -42,6 +42,7 @@ class CSCTriggerPrimitivesProducer : public edm::EDProducer
   int iev; // event number
   edm::InputTag compDigiProducer_;
   edm::InputTag wireDigiProducer_;
+  edm::InputTag gemPadProducer_;
   // swich to force the use of parameters from config file rather then from DB
   bool debugParameters_;
   // switch to for enabling checking against the list of bad chambers
